403bbfe852a215bd37134809dfec149939ba8d64
felipemaifredo <felipemaifredo@gmail.com>
2023-11-28 20:36:05 -0300

    att

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ server.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000
 
 server.get('/', (req, res) => {
-    return res.json({mensagem: 'Server is running!'})
+    const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Meu Servidor</title>
+            </head>
+            <body>
+                <h1>Bem-vindo ao meu servidor!</h1>
+                <p>O servidor está funcionando corretamente.</p>
+            </body>
+        </html>
+    `;
+    res.send(htmlContent);
 })
 
 server.post('/send-email/:email', (req, res) => {
