449984941d2e6adb3708c6d556a4d4c91442780d
felipemaifredo <felipemaifredo@gmail.com>
2024-03-31 18:52:22 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
-const { validationResult, check } = require('express-validator');
+const express = require("express")
+const bodyParser = require("body-parser")
+const nodemailer = require("nodemailer")
+const { validationResult, check } = require("express-validator")
+require("dotenv").config()
 
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
 
-const server = express();
-server.use(bodyParser.urlencoded({ extended: true }));
+const server = express()
+server.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
         user: user,
         pass: pass
     }
-});
+})
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
     const htmlContent = `
         <!DOCTYPE html>
         <html>
@@ -34,51 +34,57 @@ server.get('/', (req, res) => {
                 <p>O servidor está funcionando corretamente.</p>
             </body>
         </html>
-    `;
-    res.send(htmlContent);
-});
+    `
+    res.send(htmlContent)
+})
 
-server.post('/send-email/:email', [
-    check('name').notEmpty().withMessage('O campo nome é obrigatório'),
-    check('email').isEmail().withMessage('Informe um e-mail válido'),
-    check('message').notEmpty().withMessage('O campo mensagem é obrigatório')
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+server.post("/send-email/:email", (req, res) => {
+    const formData = req.body
+    const recipientEmail = req.params.email
+    let mailBody = ''; // Inicializa o corpo do e-mail
+
+    // Verifica se há campos no formulário
+    if (Object.keys(formData).length === 0) {
+        return res.status(400).json({ message: 'Nenhum campo foi enviado.' });
     }
 
-    const formData = req.body;
-    const recipientEmail = req.params.email;
+    // Itera sobre os campos recebidos no corpo da requisição
+    Object.keys(formData).forEach(key => {
+        mailBody += `
+            <div>
+                <p style="color: white"> 
+                    <strong>${key}:</strong> ${formData[key]} 
+                </p>
+            </div>
+        `
+    })
 
     const mailOptions = {
         from: `Felipe <${user}>`,
         to: recipientEmail,
-        subject: 'Novo Contato Recebido!',
+        subject: "Novo Contato Recebido!",
         text: formData.message,
         html: `
             <div style="background-color: black">
-                <p style="color: white">
-                    A pessoa ${formData.name}, enviou: ${formData.message}, o email dela é: ${formData.email}
-                </p>
+                ${mailBody}
             </div>
         `
-    };
+    }
 
     transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
-            console.error('Erro ao enviar o e-mail:', err);
-            return res.status(500).json({ success: false, message: 'Erro ao enviar o e-mail.' });
+            console.error("Erro ao enviar o e-mail:", err)
+            return res.status(500).json({ success: false, message: "Erro ao enviar o e-mail." })
         }
-        console.log('E-mail enviado:', info.response);
-        res.status(200).json({ success: true, message: 'E-mail enviado com sucesso!' });
-    });
-});
+        console.log("E-mail enviado:", info.response)
+        res.status(200).json({ success: true, message: "E-mail enviado com sucesso!" })
+    })
+})
 
 server.use((req, res) => {
-    res.status(404).send('Página não encontrada');
-});
+    res.status(404).send("Página não encontrada")
+})
 
 server.listen(port, () => {
-    console.log('Server is running');
-});
+    console.log("Server is running")
+})
