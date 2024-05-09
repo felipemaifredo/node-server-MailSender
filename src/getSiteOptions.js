92aae1e32ccad8b0bfb0fdbaee042fdae90a82e5
felipemaifredo <felipemaifredo@gmail.com>
2024-05-09 00:04:06 -0300

    att

--- src/getSiteOptions.js
+++ src/getSiteOptions.js
@@ -5,33 +5,66 @@ function getSiteOptions(req, res) {
     const siteConfigs = {
         "localhost:3000": {
             logo: "https://e-bordados.net/assets/images/e-bordados.png",
-            siteConfigs: [
-                {lang: "pt"}, {classToCss: "ebo"},
-            ],
-            colors: ["#ff7101", "#fff"],
+            siteConfigs:{
+                lang: "pt", classToCss: "ebo",
+            },
+            colors: {
+                primary: { key: '--c-primary', color: '#ff8531' },
+                secondary: { key: '--c-secondary', color: '#0069d9' },
+                third: { key: '--c-third', color: '#9f9d96' },
+                fourty: { key: '--c-fourty', color: '#f0efe9' },
+                fifty: { key: '--c-fifty', color: '#f0efe9' },
+                sixty: { key: '--c-sixty', color: '#000' },
+                seventy: { key: '--c-seventy', color: '#000' }
+            },
             products: ["product1", "product2", "product3"]
         },
         "new-ebordados-app-nextjs.vercel.app": {
             logo: "https://e-bordados.net/assets/images/e-bordados.png",
-            siteConfigs: [
-                {lang: "pt"}, {classToCss: "ebo"},
-            ],
-            colors: ["#ff7101", "#fff"],
+            siteConfigs: {
+                lang: "pt", classToCss: "ebo",
+            },
+            colors: {
+                primary: { key: '--c-primary', color: '#ff8531' },
+                secondary: { key: '--c-secondary', color: '#0069d9' },
+                third: { key: '--c-third', color: '#9f9d96' },
+                fourty: { key: '--c-fourty', color: '#f0efe9' },
+                fifty: { key: '--c-fifty', color: '#f0efe9' },
+                sixty: { key: '--c-sixty', color: '#000' },
+                seventy: { key: '--c-seventy', color: '#000' }
+            },
             products: ["product1", "product2", "product3"]
         },
         "localhost:3001": {
             logo: "https://embforlife.com/src/images/embforlife_logo.png",
-            siteConfigs: [
-                {lang: "en"}, {classToCss: "emb"},
-            ],
-            colors: ["#ff00ff", "#00ffff"],
+            siteConfigs: {
+                lang: "en", classToCss: "emb",
+            },
+            colors: {
+                primary: { key: '--c-primary', color: '#0069d9' },
+                secondary: { key: '--c-secondary', color: '#ff8531' },
+                third: { key: '--c-third', color: '#9f9d96' },
+                fourty: { key: '--c-fourty', color: '#f0efe9' },
+                fifty: { key: '--c-fifty', color: '#f0efe9' },
+                sixty: { key: '--c-sixty', color: '#000' },
+                seventy: { key: '--c-seventy', color: '#000' }
+            },
             products: ["product7", "product8", "product9"]
         },
         "new-ebordados-app-nextjs-7ly8.vercel.app": {
             logo: "https://embforlife.com/src/images/embforlife_logo.png",
-            siteConfigs: [
-                {lang: "en"}, {classToCss: "emb"},
-            ],
+            siteConfigs: {
+                lang: "en", classToCss: "emb",
+            },
+            colors: {
+                primary: { key: '--c-primary', color: '#0069d9' },
+                secondary: { key: '--c-secondary', color: '#ff8531' },
+                third: { key: '--c-third', color: '#9f9d96' },
+                fourty: { key: '--c-fourty', color: '#f0efe9' },
+                fifty: { key: '--c-fifty', color: '#f0efe9' },
+                sixty: { key: '--c-sixty', color: '#000' },
+                seventy: { key: '--c-seventy', color: '#000' }
+            },
             colors: ["#ff00ff", "#00ffff"],
             products: ["product7", "product8", "product9"]
         },
